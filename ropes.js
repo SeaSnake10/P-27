ad54944f39378cdff4e44e78f06305bb7e31d4dd
SeaSnake10 <76809064+SeaSnake10@users.noreply.github.com>
2021-02-18 15:50:10 +0800

    Add files via upload

--- ropes.js
+++ ropes.js
@@ -1,18 +1,31 @@
 class binds{
-    constructor(body1, body2, offsetX, offsetY){
-        this.offsetX=offsetX
-        this.offsetY=offsetY
+    constructor(body1, body2, offsetX, offsetY)
+    {
+        this.offsetX=offsetX+400;
+        this.offsetY=offsetY;
         var options = {
             bodyA:body1,
             bodyB:body2,
+            pointB:{x:offsetX, y:offsetY},
             stiffness:0.04,
-            length:10,
-            pointB:{x:this.offsetX, y:this.offsetY}
+            length:200
         }
-        this.rope = Constraint.create(options)
+        this.rope= Constraint.create(options);
         World.add(world, this.rope)
        }
        display(){
-           
+          var pointA=this.rope.bodyA.position;
+          var pointB=this.rope.bodyB.position;
+
+          strokeWeight(20);
+
+          var Anchor1X=pointA.x
+          var Anchor1Y=pointA.y;
+
+          var Anchor2X=pointB+this.offsetX;
+          var Anchor2Y=pointB+this.offsetY;
+
+          line(Anchor1X, Anchor1Y, Anchor2X, Anchor2Y,);
+          
        }
 }
--- sketch.js
+++ sketch.js
@@ -27,7 +27,12 @@ function setup() {
    ground = new frames(400,700,800,20)
    topwall = new frames(400,200,260,10)
 
-   bound1 = new binds(bob1.body,topwall.body,-bobDiameter*2, 0)
+   bound1 = new binds(bob1.body,topwall.body, -bob1.width*2, 0);
+   bound2 = new binds(bob2.body,topwall.body, -bob2.width*2+50, 0);
+   bound3 = new binds(bob3.body,topwall.body, -bob3.width*2+100, 0);
+   bound4 = new binds(bob4.body,topwall.body, -bob4.width*2+150, 0);
+   bound5 = new binds(bob5.body,topwall.body, -bob5.width*2+200, 0);
+
 
 	Engine.run(engine);
   
@@ -36,7 +41,8 @@ function setup() {
 
 function draw() {
   rectMode(CENTER);
-  background(0);
+  background(225);
+  strokeWeight(2);
   bob1.display();
   bob2.display();
   bob3.display();
@@ -50,4 +56,8 @@ function draw() {
 }
 
 
-
+function keyPressed(){
+	if(keyCode === UP_ARROW){
+		Matter.Body.applyForce(bob1.body, bob1.body.position, {x:-65,y:60});
+	}
+}
